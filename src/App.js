50605a5f62d8ed4127f6c805e56a930a36478ca8
unknown <jacksantos1408@gmail.com>
2021-07-24 16:13:31 -0300

    desafio 2 hiring coders v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import { useState, useEffect } from 'react';
+import icon_cart from './assets/cart.png';
 
 function App() {
+
+  const [storageData, setStorageData] = useState([]);
+
+
+  useEffect(() => {
+    const storageProd = JSON.parse(localStorage.getItem('storageProd'));
+
+    if(storageProd !== null && storageProd !== undefined){
+      console.log(storageProd)
+      setStorageData(storageProd.length);
+    }else{
+      setStorageData(0);
+    }
+  });
+
+  
+
+  const products = data().products;
+
+  function adicionar(product){
+    console.log("adicionei")
+    let newStorage = [];
+    if(storageData>0){
+      newStorage= JSON.parse(localStorage.getItem('storageProd'));
+    }
+    newStorage.push(product);
+    setStorageData(newStorage.length);
+    localStorage.setItem('storageProd', JSON.stringify(newStorage));
+  } 
+
+  function clearStorage(){
+    localStorage.clear();
+    setStorageData(0);
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <a className="App-header-logo" href="/">House of Funko</a>
+          <div className="App-header-basket">
+
+            <div>
+              <img src={icon_cart} width="40px" height="35px" alt="" />
+              <a href="">
+                {storageData} Itens
+              </a>
+            </div>
+
+            <div>
+              <a href="" onClick={() => clearStorage()}>Esvaziar</a>
+            </div>                     
+                       
+                        
+          </div>
+        </div>
+       
       </header>
+
+      <div className="App-main">
+
+      <nav className="App-nav">
+        <div>
+          <h3>Filtros</h3>
+          <input type="search" id="" placeholder="Pesquisar"/>
+        </div>
+        
+        <h5>Categorias</h5>
+       
+        <a href="">Filmes</a>
+        <a href="">Séries</a>
+        <a href="">Animações 3</a>
+        
+        <h5>Mais vendidos</h5>
+        
+        <a href="">Harry Potter</a>
+        <a href="">Star Wars</a>
+        <a href="">Heróis da Marvel</a>
+        <a href="">Heróis da DC</a>
+      </nav>
+      
+      <div className="App-container">
+
+        <div className="App-container-header">
+          <h1>HARRY POTTER</h1>
+        </div>
+       
+
+        <div className="App-container-content">
+
+          {
+           products.map(item => {
+             return(
+              <div className="App-content-item">
+                <img src={item['url-img']}
+                alt={item.name} width="300px" height="200px" />
+                <div><h4>{item.name}</h4></div>
+                <p>
+                  De {item.price} por
+                  <br />
+                  <span>R$ {(item.price-(item.price*item.discount)).toFixed(2)}</span>
+                </p>
+                <button onClick={() => adicionar(item)}>ADICIONAR AO CARRINHO</button>
+              </div>
+             )
+           })
+          }      
+                     
+
+                   
+        </div>
+        <div className="App-page-ctrl">
+        <div>
+          <button disabled>Voltar</button>
+          <button id="App-page-act">1</button>
+          <button >2</button>
+          <button>Avançar</button>
+        </div>
+        </div>
+       
+         
+      </div>
+
+       
+      </div>
+      <footer className="App-footer">
+        <p>
+           Copyright 2021 - Desenvolvido por Jaqueline Santos
+        </p>
+        
+      </footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,74 @@
+const storage ={
+
+  "products": [
+    {
+      "id": 1,
+      "name": "HERMIONE GRANGER - HARRY POTTER",
+      "price": 99.99,
+      "discount": 0.1,
+      "url-img": "https://cdn.awsli.com.br/600x450/1244/1244594/produto/58518611/7d69158223.jpg",
+    },
+
+    {
+      "id": 2,
+      "name": "HARRY POTTER",
+      "price": 99.99,
+      "discount": 0.1,
+      "url-img": "https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202003/11/00183180125078____1__1200x1200.jpg",
+    },
+
+    {
+      "id": 3,
+      "name": "RON WEASLY - HARRY POTTER",
+      "price": 99.99,
+      "discount": 0.1,
+      "url-img": "https://images-americanas.b2w.io/produtos/01/00/img/2520794/8/2520794801_1GG.jpg",
+    },
+
+    {
+      "id": 4,
+      "name": "ALVO DUMBLEDORE - HARRY POTTER",
+      "price": 89.99,
+      "discount": 0.1,
+      "url-img": "https://images-shoptime.b2w.io/produtos/01/00/sku/15656/7/15656709_2GG.jpg",
+    },
+
+    {
+      "id": 5,
+      "name": "SEVERO SNAPE - HARRY POTTER",
+      "price": 89.99,
+      "discount": 0.1,
+      "url-img": "https://images-americanas.b2w.io/produtos/01/00/img/2556509/1/2556509128_1GG.jpg",
+    },
+
+    {
+      "id": 6,
+      "name": "HAGRID - HARRY POTTER",
+      "price": 89.99,
+      "discount": 0.1,
+      "url-img": "https://c1-ebgames.eb-cdn.com.au/merchandising/images/packshots/d0ff3da987e24134b064960f15ad5d95_Large.jpg",
+    },
+
+    {
+      "id": 7,
+      "name": "DOBBY - HARRY POTTER",
+      "price": 79.99,
+      "discount": 0.1,
+      "url-img": "http://www.minimundi.com.br/cdn/imagens/produtos/det/dobby-harry-potter-pop-17-funko-06561_a.jpg",
+    },
+
+    {
+      "id": 1,
+      "name": "LORD VOLDEMORT - HARRY POTTER",
+      "price": 99.99,
+      "discount": 0.1,
+      "url-img": "https://a-static.mlcdn.com.br/1500x1500/funko-pop-harry-potter-voldemort-85-exclusivo-pop-in-a-box/fusaogeekpresentes/9162450258/95f9470ce07e71b00cada9ac6a9c8ec1.jpg",
+    },
+
+
+  ]
+}
+
+export default function data(){
+  return storage;
+}
